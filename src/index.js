471d7324a77631070adf11d3497596ce00308fe4
vzletit <munnharpa@gmail.com>
2022-07-26 17:21:28 +0300

    readme.md upadted

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 /* eslint-disable react/jsx-filename-extension */
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import reportWebVitals from './reportWebVitals';
+// import reportWebVitals from './reportWebVitals';
 import init from './index.jsx';
 
 const chatApp = async () => {
